37a775ef9b3b7ab81b28cf9ee4debd86a81993d1
Timon <timon@riegerx.de>
2024-11-05 03:12:23 +0100

    fix5

--- install.sh
+++ install.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+
+# Install requirements
+pip install -r requirements.txt
+
 # Check if GITHUB_TOKEN environment variable is set
 if [ -z "$GITHUB_TOKEN" ]; then
   echo "Error: GITHUB_TOKEN environment variable is not set."
@@ -8,6 +12,3 @@ fi
 
 # Install the package from GitHub
 pip install git+https://$GITHUB_TOKEN@github.com/timonrieger/database-service.git
-
-# Install requirements
-pip install -r requirements.txt
